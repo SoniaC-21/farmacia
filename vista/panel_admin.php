710d77c9e513875d251fdead1cd73487ec3b337c
Arely-19 <1911621j@umich.mx>
2025-11-29 21:27:30 -0600

    Enlace para salir al login del admin(detalle que se me olvido)

--- vista/panel_admin.php
+++ vista/panel_admin.php
@@ -42,6 +42,10 @@
 
             <button type="submit" name="registrar_empleado">Registrar Empleado</button>
         </form>
+        <div class="enlace">
+            <br><a href="../vista/admin.php">Salir</a>
+        </div>
+
 
     </div>
 
